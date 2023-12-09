ffda310931a6a98ea44cbe44404b3c31afe91de5
yeedatoy <52849956+yeedatoy@users.noreply.github.com>
2023-12-09 00:17:25 -0500

    Update and rename twrp_redfin.mk to twrp_bramble.mk

--- twrp_bramble.mk
+++ twrp_bramble.mk
@@ -2,14 +2,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from redfin device
-$(call inherit-product, device/google/redfin/device.mk)
+# Inherit from bramble device
+$(call inherit-product, device/google/bramble/device.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := twrp_redfin
+PRODUCT_NAME := twrp_bramble
 PRODUCT_BRAND := google
-PRODUCT_MODEL := Pixel 5
-PRODUCT_NAME := twrp_redfin
+PRODUCT_MODEL := Pixel 4a5g
+PRODUCT_NAME := twrp_bramble
